e0e20dc6c40bd9a0da67d28c05837e7292de2e23
keegal <darkkrage@gmail.com>
2022-02-20 16:36:54 +0000

    removes cleaning from listen.js

--- listen.js
+++ listen.js
@@ -3,6 +3,5 @@ const Group = require("./Schemas/Group");
 const User = require("./Schemas/User");
 
 app.listen(9090, async () => {
-  await Group.deleteMany({ contact: { name: "Steve", email: 'Steve@group.com' } })
   console.log("listening");
 });
