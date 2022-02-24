f0c8b7746d15e90ad50ea63be69adccb8e3d88d2
stanleyh1 <hannahstanley63@gmail.com>
2022-02-24 15:48:14 +0000

    moves login function to routers, updates endpoints.json

--- listen.js
+++ listen.js
@@ -3,7 +3,14 @@ const Group = require("./Schemas/Group");
 const User = require("./Schemas/User");
 
 const { PORT = 9090 } = process.env;
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(PORT, async () => {
+  // const query = await User.find({ username: 'Steve'})
+
+  // query[0].venues.push("6213739ee4ff4e521a587e2b")
+  // await query[0].save()
+  console.log(`Listening on ${PORT}...`)
+})
+
 
   // await User.create({
   //   username: 'Billie',
@@ -18,8 +25,8 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
   // query[0].group = ["620fc4a5ad45abc5b6ee6547"];
   // await query[0].save();
 
-  // const query = await User.find({ usernme: 'Steve'})
+  // const query = await User.find({ username: 'Steve'})
 
-  // query[0].venues = ['620fbd088a86c442a7083532']
+  // query.venues.push("6213739ee4ff4e521a587e2b")
   // await query[0].save();
 
