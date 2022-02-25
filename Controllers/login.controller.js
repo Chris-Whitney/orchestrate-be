a274ad0523d75912039c9ebbaf28641527bd8b4d
mavie1322 <babimbilyoness@gmail.com>
2022-02-25 15:42:05 +0000

    small change

--- Controllers/login.controller.js
+++ Controllers/login.controller.js
@@ -1,24 +1,23 @@
-const { fetchSingleUser } = require('../Models/users.model')
+const { fetchSingleUser } = require("../Models/users.model");
 
-const passport = require('passport')
+const passport = require("passport");
 
 exports.loginFail = (req, res, next) => {
-    res.status(200).send({ msg: 'Login Failed'})
-}
+  res.status(200).send({ msg: "Login Failed" });
+};
 
 exports.loginPass = (req, res, next) => {
-    const userId = {userId : req.session.passport.user}
-    fetchSingleUser(userId).then((user) => {
-    res.status(200).send({ msg: 'Success', user: user })
-})
-}
+  const userId = { userId: req.session.passport.user };
+  // fetchSingleUser(userId).then((user) => {
+  res.status(200).send({ msg: "Success", user: req });
+  // })
+};
 
 exports.fetchLogin = (req, res, next) => {
-    res.sendFile(__dirname + "/form.html");
-    }
+  res.sendFile(__dirname + "/form.html");
+};
 
 exports.checkLogin = passport.authenticate("local", {
-        successRedirect: "/login/pass",
-        failureRedirect: "/login/fail",
-    })
-        
+  successRedirect: "/login/pass",
+  failureRedirect: "/login/fail",
+});
