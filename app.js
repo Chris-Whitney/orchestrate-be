48f542aee7d67427c6ceaac97e2e009cdfa7f6f2
keegal <darkkrage@gmail.com>
2022-02-26 11:11:30 +0000

    login testing

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(function (req, res, next) {
     next();
   }
 });
-app.use(cors());
+var corsOptions = {
+  origin: 'localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
