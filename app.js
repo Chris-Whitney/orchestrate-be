83c3a6dc6d36d6b6a9b5e06c62367f7fc706e07c
keegal <darkkrage@gmail.com>
2022-02-26 10:56:11 +0000

    allows credentials cross origin

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const loginRouter = require('./routers/login.router');
 const apiRouter = require('./routers/api.router');
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
